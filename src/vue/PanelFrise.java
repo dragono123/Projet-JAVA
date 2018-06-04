417c2e0cf2d8cf3ba68461b50bbafa271e0ecfe8
lim vincent <vlim@(none)>
2018-06-04 09:34:59 +0200

    Ajout de PanelFrise

--- src/vue/PanelFrise.java
+++ src/vue/PanelFrise.java
@@ -1,8 +1,21 @@
 package vue;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 
 public class PanelFrise extends JPanel
 {
-
+	public PanelFrise()
+	{
+		setLayout(new BorderLayout(10, 5));
+		PanelDescription panelDesc = new PanelDescription();
+		PanelEvts panelEvts = new PanelEvts();
+		
+		
+		setLayout(panelDesc, BorderLayout.NORTH);
+		setLayout(panelEvts, BorderLayout.SOUTH);
+	}
+	
+	
 }
